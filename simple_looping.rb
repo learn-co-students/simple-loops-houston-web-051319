c9e67cfad09046bec5058b44b9a7a47a5a8f8084
Aaron B <abriscoe2008@gmail.com>
2019-05-16 22:47:56 +0000

    Done.

--- simple_looping.rb
+++ simple_looping.rb
@@ -1,24 +1,11 @@
-# REMEMBER: print your output to the terminal using 'puts'
-
 def loop_iterator(number_of_times)
-  number_of_times = 0
-  loop do
-    number_of_times += 1
+  counter = 0
+  loop do 
     puts "Welcome to Flatiron School's Web Development Course!"
-    if number_of_times == number_of_times
-      break
-    end
+    counter += 1
+    break if counter == number_of_times
   end
 end
-  # Code your solution here using the "loop" keyword to puts out the below phrase
-  # This and all the other methods take an argument of an integer 
-  # The integer is the number of times the loops should puts out the phrase
-  # How can we make sure the loop breaks once it has puts out the phrase the 
-  #   correct number of times?
-  # Maybe we should keep count of the number of times we've puts out the 
-  #  phrase and break when the counter hits the appropriate number...
-
-
 
 def times_iterator(number_of_times)
   number_of_times.times do 
@@ -26,26 +13,28 @@ def times_iterator(number_of_times)
   end
 end
 
-def while_iterator(number_of_times)
-   number_of_times = 0
-  while number_of_times  number_of_times
-    puts "Welcome to Flatiron School's Web Development Course!"
-    number_of_times += 1 
+def while_iterator(number_of_times) 
+  count = 1 
+  while count <= number_of_times 
+    puts "Welcome to Flatiron School's Web Development Course!" 
+    count += 1 
   end
 end
 
 def until_iterator(number_of_times)
-  # code your solution here using the "until" keyword
-  # hint: use a counter to tell the until loop when to stop!
+  counter = 0
   phrase = "Welcome to Flatiron School's Web Development Course!"
-
+  until counter == number_of_times
+    puts phrase
+    counter += 1 
+  end 
 end
 
 def for_iterator(number_of_times)
-  # code your solution here using the "for" keyword
-  # remember that `for` requires a range of numbers. How can we turn our number into a range?
-  
+  count = 1..number_of_times
   phrase = "Welcome to Flatiron School's Web Development Course!"
-  
+  for number_of_times in count
+    puts phrase
+  end
 end
 
